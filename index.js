6eee74e5c3ba2b757eb0787ff4761d5c0e4fc27c
Damien Lebrun <dinoboff@hotmail.com>
2016-03-23 14:45:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,260 @@
+'use strict';
+
+const argparse = require('argparse');
+const camelCase = require('lodash.camelcase');
+const fs = require('fs');
+
+class Settings {
+
+  constructor(defaults) {
+    Object.assign(this, defaults);
+  }
+
+  /**
+   * Update seetings with environment variables.
+   *
+   *  options:
+   *
+   *  - filter: a function filtering keys. Take the key as argument and must
+   *    return true if the key should included (optional);
+   *  - include: array of key to include (optional);
+   *  - exclude: array of key to exclude (optional).
+   *
+   * @param  {string}  prefix
+   * @param  {object}  opts
+   * @return {promise}
+   */
+  env(prefix, opts) {
+    prefix = prefix || '';
+    opts = opts || {};
+
+    return new Promise(resolve => {
+      const env = opts.env || process.env;
+      const prefixLength = prefix.length;
+
+      const vars = entries(env).filter(
+        entrie => entrie[0].startsWith(prefix)
+      );
+
+      opts.transform = (entrie) => ([
+        camelCase(entrie[0].slice(prefixLength)),
+        entrie[1]
+      ]);
+
+      resolve(this._update(vars, opts));
+    });
+  }
+
+  /**
+   * Update settings with json encoded settings from a file.
+   *
+   * options:
+   * - transform: function transforming the key and value of each item in the
+   *   json file (optional);
+   * - filter: a function filtering keys. Take the key as argument and must
+   *   return true if the key should included (optional);
+   * - include: array of key to include (optional);
+   * - exclude: array of key to exclude (optional).
+   *
+   * @param  {string} path
+   * @param  {object} opts
+   * @return {promise}
+   */
+  json(path, opts) {
+    return readJsonFile(path).then(
+      settings => this._update(entries(settings), opts)
+    );
+  }
+
+  /**
+   * Update setting using a argparse parser.
+   *
+   * options:
+   * - version;
+   * - descriptions;
+   * - epilog;
+   * - cmd: the cli main command (either cmd or subCmds can be defined)
+   * - subCmds: array of subCmd object (each define a name, a description, an
+   *   options function and cmd function).
+   * - options: function extending parsers.
+   * - filter: a function filtering keys. Take the key as argument and must
+   *   return true if the key should included (optional);
+   * - include: array of key to include (optional);
+   * - exclude: array of key to exclude (optional).
+   *
+   * @param  {object}          opts
+   * @param  {array|undefined} args uses process.argv by default
+   * @return {promise}
+   */
+  argv(opts) {
+    return new Promise(resolve => {
+      const argv = opts.argv;
+      const parser = parserFactory(opts, this);
+
+      resolve(parser.parseArgs(argv));
+    }).then(settings => {
+      opts.transform = (entrie) => ([
+        camelCase(entrie[0]),
+        entrie[1]
+      ]);
+
+      return this._update(entries(settings), opts);
+    });
+  }
+
+  update(settings, opts) {
+    return this._update(entries(settings), opts);
+  }
+
+  _update(entries, opts) {
+    const cfg = entries.map(
+      transformFactory(opts)
+    ).filter(
+      filterFactory(opts)
+    );
+
+    return Object.assign(this, obj(cfg));
+  }
+}
+
+function entries(obj) {
+  return Object.keys(obj).map(
+    k => ([k, obj[k]])
+  );
+}
+
+function obj(entries) {
+  return entries.reduce((obj, e) => {
+    obj[e[0]] = e[1];
+
+    return obj;
+  }, {});
+}
+
+function transformFactory(opts) {
+  if (opts && opts.transform) {
+    return opts.transform;
+  }
+
+  return e => e;
+}
+
+function filterFactory(opts) {
+  opts = opts || {};
+
+  if (opts.filter) {
+    return opts.filter;
+  }
+
+  if (opts.exclude) {
+    const exclude = new Set(opts.exclude);
+
+    return entrie => !exclude.has(entrie[0]);
+  }
+
+  if (opts.include) {
+    const include = new Set(opts.include);
+
+    return entrie => include.has(entrie[0]);
+  }
+
+  return () => true;
+}
+
+function parserFactory(opts, defaults) {
+  if (!opts || !opts.cmd && (!opts.subCmds || !opts.subCmds.length)) {
+    throw new Error('opts.cmd or opts.subCmds must be set.');
+  }
+
+  const parser = new argparse.ArgumentParser({
+    version: opts.version || '0.0.0',
+    addHelp: opts.addHelp === false ? false : true,
+    description: opts.description || '',
+    formatterClass: argparse.RawTextHelpFormatter,
+    epilog: opts.epilog
+  });
+
+  parser.addArgument(['-d', '--debug' ], {
+    action: 'storeTrue',
+    help: 'print debug messages'
+  });
+
+  parser.addArgument(['-s', '--silent' ], {
+    action: 'storeTrue',
+    help: 'print only error messages'
+  });
+
+  if (opts.options) {
+    opts.options(parser, defaults);
+  }
+
+  if (opts.cmd) {
+    parser.setDefaults({cmd: opts.cmd});
+    return parser;
+  }
+
+  const subparsers = parser.addSubparsers();
+
+  opts.subCmds.forEach(sub => {
+    const parser =  subparsers.addParser(sub.name, {
+      addHelp: true,
+      description: sub.description,
+      formatterClass: argparse.RawTextHelpFormatter
+    });
+
+    parser.setDefaults({cmd: sub.cmd});
+    sub.options(parser, defaults);
+  });
+
+  return parser;
+}
+
+function readJsonFile(path) {
+  return readFile(path).catch(
+    () => '{}'
+  ).then(
+    content => JSON.parse(content)
+  );
+}
+
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function writeFile(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function deleteFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(path, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+exports.Settings = Settings;
+exports.settings = defaults => new Settings(defaults);
+exports.readFile = readFile;
+exports.writeFile = writeFile;
+exports.deleteFile = deleteFile;
---
+++ tests/index.js
@@ -0,0 +1,211 @@
+'use strict';
+
+const cli = require('../');
+const expect = require('expect.js');
+const path = require('path');
+
+describe('Settings', function() {
+  let settings;
+
+  beforeEach(function() {
+    settings = cli.settings({foo: 1});
+  });
+
+  it('should take default values', function() {
+    expect(settings).to.eql({foo: 1});
+  });
+
+  describe('update', function() {
+
+    it('should filter entries', function() {
+      const foo = 2;
+      const bar = 3;
+
+      settings.update({foo, bar}, {
+        filter: entrie => entrie[1] !== 3
+      });
+
+      expect(settings.foo).to.be(foo);
+      expect(settings.bar).to.be(undefined);
+    });
+
+    it('should exclude by key', function() {
+      const foo = 2;
+      const bar = 3;
+
+      settings.update({foo, bar}, {
+        exclude: ['bar']
+      });
+
+      expect(settings.foo).to.be(foo);
+      expect(settings.bar).to.be(undefined);
+    });
+
+    it('should include by key', function() {
+      const foo = 2;
+      const bar = 3;
+      const baz = 4;
+
+      settings.update({foo, bar, baz}, {
+        include: ['foo']
+      });
+
+      expect(settings.foo).to.be(foo);
+      expect(settings.bar).to.be(undefined);
+      expect(settings.baz).to.be(undefined);
+    });
+
+  });
+
+  describe('env', function() {
+    let origEnv;
+
+    beforeEach(function() {
+      origEnv = process.env;
+    });
+
+    afterEach(function() {
+      process.env = origEnv;
+    });
+
+    it('should update setting en env. variables', function() {
+      const prefix = 'BAR_';
+      const foo = 2;
+      const fooBar = 3;
+      const env = {
+        [`${prefix}FOO`]: foo,
+        [`${prefix}FOO_BAR`]: fooBar,
+        'FOO_BAR': 4
+      };
+
+      return settings.env(prefix, {env}).then(settings => {
+        expect(settings).to.eql({foo, fooBar});
+      });
+    });
+
+    it('should have "" has default prefix', function() {
+      const prefix = '';
+      const foo = 2;
+      const fooBar = 3;
+      const env = {
+        [`${prefix}FOO`]: foo,
+        [`${prefix}FOO_BAR`]: fooBar
+      };
+
+      return settings.env(undefined, {env}).then(settings => {
+        expect(settings).to.eql({foo, fooBar});
+      });
+    });
+
+    it('should use process.env by default', function() {
+      const prefix = 'BAR_';
+      const foo = 2;
+      const fooBar = 3;
+
+      process.env = {
+        [`${prefix}FOO`]: foo,
+        [`${prefix}FOO_BAR`]: fooBar,
+        'FOO_BAR': 4
+      };
+
+      return settings.env(prefix).then(settings => {
+        expect(settings).to.eql({foo, fooBar});
+      });
+    });
+
+  });
+
+  describe('json', function() {
+    const cfgPath = path.join(__dirname, 'cfg.json');
+    const notFoundPath = path.join(__dirname, 'notfound.json');
+    const foo = 2;
+    const fooBar = 3;
+
+    beforeEach(function() {
+      return cli.writeFile(cfgPath, JSON.stringify({foo, fooBar}));
+    });
+
+    afterEach(function() {
+      return cli.deleteFile(cfgPath);
+    });
+
+    it('should update setting from json file', function() {
+
+      return settings.json(cfgPath).then(settings => {
+        expect(settings).to.eql({foo, fooBar});
+      });
+    });
+
+    it('should skip json file update if file is missing', function() {
+      return settings.json(notFoundPath).then(settings => {
+        expect(settings).to.eql({foo: 1});
+      });
+    });
+
+  });
+
+  describe('argv', function() {
+
+    it('should update settings from argv', function() {
+      const foo = 2;
+      const fooBar = '3';
+      const argv = ['--foo', '' + foo, '--foo-bar', fooBar];
+      const options = (parser) => {
+        parser.addArgument(['-f', '--foo'], {type: 'int'});
+        parser.addArgument(['-b', '--foo-bar']);
+      };
+      const cmd = () => undefined;
+      const addHelp = false;
+
+      return settings.argv({argv, cmd, options, addHelp}).then(settings => {
+        expect(settings).to.eql({
+          foo,
+          fooBar,
+          cmd,
+          debug: false,
+          silent: false
+        });
+        expect(settings.foo).to.be(2);
+      });
+    });
+
+    it('should parse sub commands', function() {
+      const foo = 2;
+      const fooBar = '3';
+      const argv = ['cmd1', '--foo', '' + foo, '--foo-bar', fooBar];
+      const cmd = () => undefined;
+      const subCmds = [{
+        name: 'cmd1',
+        options: (parser) => {
+          parser.addArgument(['-f', '--foo'], {type: 'int'});
+          parser.addArgument(['-b', '--foo-bar']);
+        },
+        cmd: cmd
+      }];
+
+      return settings.argv({argv, subCmds}).then(settings => {
+        expect(settings).to.eql({
+          foo,
+          fooBar,
+          cmd,
+          debug: false,
+          silent: false
+        });
+        expect(settings.foo).to.be(2);
+      });
+    });
+
+    it('should reject if neither cmd or subCmds is not set', function() {
+      const argv = ['cmd1', '--foo', '2', '--foo-bar', '3'];
+
+      return settings.argv({argv}).then(
+        () => Promise.reject(new Error('unexpected')),
+        () => undefined
+      ).then(
+        () => expect(settings).to.eql({foo: 1})
+      );
+    });
+
+  });
+
+});
